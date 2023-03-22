0dfb0291702de2c5da18118a649e74cd7545bf3f
Caesar-12 <idowuenoch65@gmail.com>
2023-03-22 08:32:57 -0700

    created and updated a method delete and all respectively for file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,7 +18,9 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        # self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
@@ -34,6 +36,7 @@ class FileStorage:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         del(self.__objects[key])
+
     def reload(self):
         """Loads storage dictionary from file"""
         from models.base_model import BaseModel
@@ -54,6 +57,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
