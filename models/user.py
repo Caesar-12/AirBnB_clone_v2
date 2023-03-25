d62a9312b6181cf7da871b917a5e54a3b0192523
Caesar-12 <idowuenoch65@gmail.com>
2023-03-25 06:19:43 -0700

    Creating relationships between tables

--- models/user.py
+++ models/user.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
 class User(BaseModel, Base):
@@ -12,3 +15,6 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable = False)
     first_name = Column(String(128), nullable = False)
     last_name = Column(String(128), nullable = False)
+
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="user"
