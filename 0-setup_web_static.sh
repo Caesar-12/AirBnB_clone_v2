d9b650693a42ff7856e9cda2851038ed3ee1f490
idowuenoch65@gmail.com <idowuenoch65@gmail.com>
2023-04-23 17:33:23 +0100

    Added flags to avoid errors

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 # sets up your web servers for the deployment of web_static
 
-sudo apt-get install nginx
-sudo mkdir /data
-sudo mkdir /data/web_static
-sudo mkdir /data/web_static/releases
-sudo mkdir /data/web_static/shared
-sudo mkdir /data/web_static/releases/test
-sudo touch /data/web_static/releases/test/index.html
+sudo apt-get -y install nginx
+sudo mkdir -p /data
+sudo mkdir -p /data/web_static
+sudo mkdir -p /data/web_static/releases
+sudo mkdir -p /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test
+sudo touch -p /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ububtu /data
+sudo chown -R ubuntu /data/
+sudo chgrp -R ubuntu /data/
 
 sudo printf %s "server {
     listen 80 default_server;
