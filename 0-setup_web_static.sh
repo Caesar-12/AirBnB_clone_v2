658c40117c1ed045d60c21fd7143f474d1badd6b
idowuenoch65@gmail.com <idowuenoch65@gmail.com>
2023-04-23 16:48:52 +0100

    Updated setup script

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,5 +8,27 @@ mkdir /data/web_static/releases
 mkdir /data/web_static/shared
 mkdir /data/web_static/releases/test
 touch /data/web_static/releases/test/index.html
-ln data/web_static/current /data/web_static/releases/test
-chown -R /data ubuntu:ububtu
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ububtu /data
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
