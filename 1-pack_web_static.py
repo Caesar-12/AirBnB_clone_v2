e59452b2498cca4ed9b4fa29a9faa459d57f8b2a
Caesar-12 <idowuenoch65@gmail.com>
2023-04-24 04:28:04 -0700

    Fabric script to generate .tgz archive

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -8,11 +8,12 @@ from fabric.api import local
 import os
 from datetime import datetime
 
+
 def do_pack():
     """
     function to create arhive
     """
-    
+
     date = datetime.now()
     arch_name = "web_static_{}{}{}{}{}{}.tgz".format(
         date.year,
@@ -24,14 +25,15 @@ def do_pack():
         )
     arch_dir = "versions/{}".format(arch_name)
     if not os.path.isdir(arch_dir):
-        os.mkdirs("versions"):
-    
+        os.mkdirs("versions")
+
     print("Packing web_static to {}".format(arch_dir))
+
     try:
         local("tar -cvzf {} web_static".format(arch_dir))
         size = os.stat(output).st_size
         print("web_static packed: {} -> {} Bytes".format(output, size))
-    except:
+    except Exception:
         arch_dir = None
 
     return arch_dir
